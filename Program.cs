4db1110e0ad6d9c5e8d392a7b4373e6d7f2be357
Tommy Pham <tphazs@gmail.com>
2020-04-07 19:37:30 +0200

    User kinda done

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace _20183732_Tommy_Pham
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            ////Test for user.cs
+            //User test1 = new User("Tommy", "Pham", "TPhamz", "pik@kmkm.dk");
+            //User test2 = new User("Lasse", "Toet", "SinghKingh22", "pol@kasdsamkm.dk");
+            //User test3 = new User("Kasper", "Singh", "Comestink", "s@kasdsamkm.dk");
+
+            //test1.ToStringTest();
+            //test2.ToStringTest();
+            //test3.ToStringTest();
+
+
         }
     }
 }
--- user.cs
+++ user.cs
@@ -1,18 +1,36 @@
 ﻿//20183732_Tommy_Pham
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace _20183732_Tommy_Pham
-{
-	public class User : IComparable
+{ 
+	// mangler Equalmetode samt gest hashcode
+	//Delegate advarsel implementation
+	//
+	public class User : IComparable<User>
     {
 		private int _id;
-		private int _firstName;
+		private string _firstName;
 		private string _lastName;
 		private string _userName;
+		private string _email;
 		private decimal _balance;
 
-		public User()
+		private static int _userID = 0;
+
+		public User(string firstName, string lastName, string userName, string email)
 		{
+			this.id = System.Threading.Interlocked.Increment(ref _userID);
+			this.firstName = firstName;
+			this.lastName = lastName;
+			this.username = userName;
+			this.email = email;
+			this.balance = 0; 
+
+
 
 		}
 
@@ -27,35 +45,90 @@ namespace _20183732_Tommy_Pham
 			get { return _id; }
 			set { _id = value; }
 		}
-		public int firstName
+		public string firstName
 		{
 			get { return _firstName; }
-			set { _firstName = value; }
+			set {
+					if (value != null)
+					{
+						_firstName = value;
+					}
+					else
+					{
+						throw new ArgumentException("The first name can not be null", "value");
+					}
+				}
 		}
 		public string lastName
 		{
 			get { return _lastName; }
-			set { _lastName = value; }
+			set {
+					if (value != null)
+					{
+						_lastName = value;
+					}
+					else
+					{
+						throw new ArgumentException("The last name can not be null", "value");
+					}
+				}
 		}
+
 		public string username
 		{
 			get { return _userName; }
-			set { _userName = value; }
+			private set 
+				{
+					string userNamePattern = @"^[a-zA-Z0-9_]+$";
+
+					if (Regex.IsMatch(value, userNamePattern))
+					{
+						_userName = value;
+					}
+					else
+					{
+						throw new ArgumentException("username contains invalid characters", "value");
+					}
+				}
+		}
+		public string email
+		{
+			get { return _email; }
+			set {
+				string emailpattern = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+					if (Regex.IsMatch(value, emailpattern))
+					{
+						_email = value;
+					}
+					else
+					{
+					throw new ArgumentException("Invalid Email", "value");
+					}
+					
+				}
 		}
 
-		public void ToString()
+		//public delegate Exception(User user, decimal balance);
+
+		public string ToString()
 		{
-			throw new NotImplementedException();
+			return "username: " + username + " email: " + email;
 		}
+
+		public void ToStringTest()
+		{
+			Console.WriteLine($"id: {id} firstname {firstName} lastname {lastName} username {username} email {email} balance {balance}");
+		}
+
 		public void Equals()
 		{
-			throw new NotImplementedException();
+			GetHashCode();
 
 		}
 
-		public int CompareTo(object obj)
+		public int CompareTo(User other)
 		{
-			throw new NotImplementedException();
+			return id.CompareTo(other.id);
 		}
 	}
 }
